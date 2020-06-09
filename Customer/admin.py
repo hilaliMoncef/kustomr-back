771c7172a9aedfd01c6ca8ef839da405ca69b29a
Hilali Moncef <hilali.moncef@gmail.com>
2020-06-09 21:13:33 +0200

    adding transactions to admin

--- Customer/admin.py
+++ Customer/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Customer, CustomerToken, CustomersList
+from .models import Customer, CustomerToken, CustomersList, Transaction
 
 admin.site.register(Customer)
+admin.site.register(Transaction)
 admin.site.register(CustomerToken)
 admin.site.register(CustomersList)
